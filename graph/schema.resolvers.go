0ac3896cd29d5979675040cff150cac627822d1b
Juan Francisco Giménez Silva <juanfgs@gmail.com>
2022-09-20 10:40:54 -0300

    implement create, add shape and query

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -13,27 +13,65 @@ import (
 )
 
 // CreateCanvas is the resolver for the createCanvas field.
-func (r *mutationResolver) addShape(ctx context.Context, input model.NewRectangle) (*model.Canvas, error) {
-	var rectangle shapes.Rectangle
-	return &model.Canvas{ID: canvasID, Name: canvas.Name}, nil
-}
-
-
-// CreateCanvas is the resolver for the createCanvas field.
 func (r *mutationResolver) CreateCanvas(ctx context.Context, input model.NewCanvas) (*model.Canvas, error) {
 	var canvas canvas.Canvas
 	canvas.Name = input.Name
-	canvasID := canvas.Save()
-	return &model.Canvas{ID: canvasID, Name: canvas.Name, Contents: canvas.Contents}, nil
+	canvasID := canvas.Create()
+	return &model.Canvas{ID: canvasID, Name: canvas.Name}, nil
+}
+
+// AddShape is the resolver for the addShape field.
+func (r *mutationResolver) AddShape(ctx context.Context, input model.NewRectangle) (*model.Canvas, error) {
+	var canvas canvas.Canvas
+	var responseShapes []*model.Rectangle
+	canvas.Get(input.CanvasID)
+	canvas.Contents = append(canvas.Contents, &shapes.Rectangle{
+		X:       input.X,
+		Y:       input.Y,
+		Width:   input.Width,
+		Height:  input.Height,
+		Fill:    input.Fill,
+		Outline: input.Outline,
+	})
+
+	canvas.Save()
+
+	for _, rectangle := range canvas.Contents {
+		responseShapes = append(responseShapes, &model.Rectangle{
+			X:       rectangle.X,
+			Y:       rectangle.Y,
+			Width:   rectangle.Width,
+			Height:  rectangle.Height,
+			Fill:    rectangle.Fill,
+			Outline: rectangle.Outline,
+		})
+	}
+
+	return &model.Canvas{ID: canvas.ID, Name: canvas.Name, Contents: responseShapes}, nil
 }
 
 // Canvases is the resolver for the canvases field.
 func (r *queryResolver) Canvases(ctx context.Context) ([]*model.Canvas, error) {
 	var canvases []*model.Canvas
-	placeholderCanvas := model.Canvas{
-		Name: "Canvas ID",
+	var canvasRectangles []*model.Rectangle
+	var canvasesData = canvas.GetAll()
+	for _, canvas := range canvasesData {
+		for _, rectangle := range canvas.Contents {
+			canvasRectangles = append(canvasRectangles, &model.Rectangle{
+				X:       rectangle.X,
+				Y:       rectangle.Y,
+				Width:   rectangle.Width,
+				Height:  rectangle.Height,
+				Fill:    rectangle.Fill,
+				Outline: rectangle.Outline,
+			})
+		}
+		canvases = append(canvases, &model.Canvas{
+			ID:       canvas.ID,
+			Name:     canvas.Name,
+			Contents: canvasRectangles,
+		})
 	}
-	canvases = append(canvases, &placeholderCanvas)
 	return canvases, nil
 }
 
